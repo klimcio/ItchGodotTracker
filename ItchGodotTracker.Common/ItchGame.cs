2616aa68ffd9618208de7db0974710d23110c162
Klimek Mariusz <klimcio@gmail.com>
2022-02-14 21:29:01 +0100

    First two unit tests checking whether the file exists or whether it can be converted

---
+++ ItchGodotTracker.Common/ItchGame.cs
@@ -0,0 +1,21 @@
+﻿namespace ItchGodotTracker.Common
+{
+    public class ItchGame
+    {
+        public string? Url { get; set; }
+        public string? Uploader { get; set; }
+        public string? Published { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? Status { get; set; }
+        public string? Platforms { get; set; }
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
+        public string? Authors { get; set; }
+        public string? MadeWith { get; set; }
+        public string? Tags { get; set; }
+        public string? AverageSession { get; set; }
+        public string? Language { get; set; }
+        public string? Inputs { get; set; }
+
+    }
+}
--- ItchGodotTracker.Tests/IsJsonAvailableTest.cs
+++ ItchGodotTracker.Tests/IsJsonAvailableTest.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
-using System.Net;
+using ItchGodotTracker.Common;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -8,16 +10,28 @@ namespace ItchGodotTracker.Tests
 {
     public class IsJsonAvailableTest
     {
+        private const string File = @"https://raw.githubusercontent.com/Godotes/itchGodotGames/main/TotalClean.json";
+
         [Fact]
         public void IsJsonAvailable()
         {
-            var file = @"https://raw.githubusercontent.com/Godotes/itchGodotGames/main/TotalClean.json";
-
             using HttpClient? client = new();
             
-            var fileContent = Task.Run(() => client.GetStringAsync(file)).Result;
+            var fileContent = Task.Run(() => client.GetStringAsync(File)).Result;
 
             fileContent.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void DoesItParseWithoutProblems()
+        {
+            using HttpClient? client = new();
+
+            var json = Task.Run(() => client.GetStringAsync(File)).Result;
+
+            var result = JsonConvert.DeserializeObject<IList<ItchGame>>(json);
+
+            result.Count.Should().NotBe(0);
         }
     }
 }
